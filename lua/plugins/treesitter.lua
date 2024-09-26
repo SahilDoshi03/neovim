3ef3d68690a4fde5028fddd4a56a7472b6b1e8ac
Sahil Doshi <sahildoshi@Sahils-MacBook-Pro.local>
2024-09-26 09:46:30 +0530

    Add auto-tag

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,36 +1,45 @@
 return {
-    {
-        "nvim-treesitter/nvim-treesitter",
-        build = ":TSUpdate",
-        config = function()
-            require'nvim-treesitter.configs'.setup {
-                ensure_installed = {
-                    "javascript", "typescript", "tsx", "c", "lua",
-                    "vim", "vimdoc", "query", "markdown", "markdown_inline"
-                },
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		config = function()
+			require("nvim-treesitter.configs").setup({
+				ensure_installed = {
+                    "html",
+                    "css",
+					"javascript",
+					"typescript",
+					"tsx",
+					"c",
+					"lua",
+					"vim",
+					"vimdoc",
+					"query",
+					"markdown",
+					"markdown_inline",
+				},
 
-                sync_install = false,
-                auto_install = true,
+				sync_install = false,
+				auto_install = true,
 
-                highlight = {
-                    enable = true,
-                    additional_vim_regex_highlighting = false,
-                },
+				highlight = {
+					enable = true,
+					additional_vim_regex_highlighting = false,
+				},
 
-                indent = {
-                    enable = true
-                },
+				indent = {
+					enable = true,
+				},
 
-                fold = {
-                    enable = true
-                }
-            }
+				fold = {
+					enable = true,
+				},
+			})
 
-            vim.opt.foldmethod = "indent"
-            vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
-            vim.opt.foldenable = true
-            vim.opt.foldlevel = 99
-        end
-    }
+			vim.opt.foldmethod = "indent"
+			vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+			vim.opt.foldenable = true
+			vim.opt.foldlevel = 99
+		end,
+	},
 }
-
