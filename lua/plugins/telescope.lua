072073742f9869e9eab5a861531ab442eb7fef0b
Sahil Doshi <sahildoshi@Sahils-MacBook-Pro.local>
2024-09-26 22:54:51 +0530

    Add fuzzy find for directories

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -9,6 +9,24 @@ return {
             vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
             vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
             vim.keymap.set("n", "<C-p>", builtin.git_files, {})
+            vim.keymap.set("n", "<leader>fd", function()
+                require("telescope.builtin").find_files({
+                    prompt_title = "Find Folders",
+                    -- Replace 'fd' with 'find' if fd is not installed or 'fd' command isn't working
+                    find_command = {
+                        "find",
+                        ".",
+                        "-type",
+                        "d",
+                        "-not",
+                        "-path",
+                        "./.git*",
+                        "-not",
+                        "-path",
+                        "./node_modules*",
+                    },
+                })
+            end, {})
         end,
     },
     {
