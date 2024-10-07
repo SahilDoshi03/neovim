eeef43b13d1f3e13caec328100ab8a3f80bb9de6
SahilDoshi03 <sahildsh03@gmail.com>
2024-10-07 13:39:04 +0530

    Navigate to undo tree when it opens

--- lua/plugins/undotree.lua
+++ lua/plugins/undotree.lua
@@ -1,9 +1,9 @@
 return {
-    {
-        "mbbill/undotree",
-        config = function()
-            vim.keymap.set('n', '<leader>u', ':UndotreeToggle<CR>')
-        end
-    }
+  {
+    "mbbill/undotree",
+    config = function()
+      vim.keymap.set("n", "<leader>u", ":UndotreeToggle<CR>")
+      vim.g.undotree_SetFocusWhenToggle = 1 
+    end,
+  },
 }
-
