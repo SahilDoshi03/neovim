376ba436baa12e1d184bc7b4556156b20dce7e5d
Sahil Doshi <sahildoshi@Sahils-MacBook-Pro.local>
2024-10-13 16:39:40 +0530

    Modify lualine to show date and time instead of progress

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,67 +1,67 @@
 return {
-    {
-        "nvim-lualine/lualine.nvim",
-        dependencies = { "nvim-tree/nvim-web-devicons" },
-        config = function()
-            -- Define the colors based on Catppuccin Mocha
-            local colors = {
-                darkgray = "#1E1E2E",    -- Mocha Dark Background
-                gray = "#6C6C85",        -- Mocha Gray
-                innerbg = 'NONE',        -- Transparent background for inner sections
-                outerbg = "#1E1E2E",     -- Mocha Dark Background
-                normal = "#F5C2E7",      -- Mocha Normal Mode Color
-                insert = "#B9FBC0",      -- Mocha Insert Mode Color
-                visual = "#F5A7B8",      -- Mocha Visual Mode Color
-                replace = "#F6D7E7",     -- Mocha Replace Mode Color
-                command = "#F9E2AF",     -- Mocha Command Mode Color
-            }
+	{
+		"nvim-lualine/lualine.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		config = function()
+			-- define the colors based on catppuccin mocha
+			local colors = {
+				darkgray = "#1e1e2e", -- mocha dark background
+				gray = "#6C6C85", -- Mocha Gray
+				innerbg = "NONE", -- Transparent background for inner sections
+				outerbg = "#1E1E2E", -- Mocha Dark Background
+				normal = "#F5C2E7", -- Mocha Normal Mode Color
+				insert = "#B9FBC0", -- Mocha Insert Mode Color
+				visual = "#F5A7B8", -- Mocha Visual Mode Color
+				replace = "#F6D7E7", -- Mocha Replace Mode Color
+				command = "#F9E2AF", -- Mocha Command Mode Color
+			}
 
-            -- Theme configuration for Lualine
-            local theme = {
-                inactive = {
-                    a = { fg = colors.gray, bg = colors.outerbg, gui = "bold" },
-                    b = { fg = colors.gray, bg = colors.outerbg },
-                    c = { fg = colors.gray, bg = colors.innerbg },
-                },
-                visual = {
-                    a = { fg = colors.darkgray, bg = colors.visual, gui = "bold" },
-                    b = { fg = colors.gray, bg = colors.outerbg },
-                    c = { fg = colors.gray, bg = colors.innerbg },
-                },
-                replace = {
-                    a = { fg = colors.darkgray, bg = colors.replace, gui = "bold" },
-                    b = { fg = colors.gray, bg = colors.outerbg },
-                    c = { fg = colors.gray, bg = colors.innerbg },
-                },
-                normal = {
-                    a = { fg = colors.darkgray, bg = colors.normal, gui = "bold" },
-                    b = { fg = colors.gray, bg = colors.outerbg },
-                    c = { fg = colors.gray, bg = colors.innerbg },
-                },
-                insert = {
-                    a = { fg = colors.darkgray, bg = colors.insert, gui = "bold" },
-                    b = { fg = colors.gray, bg = colors.outerbg },
-                    c = { fg = colors.gray, bg = colors.innerbg },
-                },
-                command = {
-                    a = { fg = colors.darkgray, bg = colors.command, gui = "bold" },
-                    b = { fg = colors.gray, bg = colors.outerbg },
-                    c = { fg = colors.gray, bg = colors.innerbg },
-                },
-            }
+			-- Theme configuration for Lualine
+			local theme = {
+				inactive = {
+					a = { fg = colors.gray, bg = colors.outerbg, gui = "bold" },
+					b = { fg = colors.gray, bg = colors.outerbg },
+					c = { fg = colors.gray, bg = colors.innerbg },
+				},
+				visual = {
+					a = { fg = colors.darkgray, bg = colors.visual, gui = "bold" },
+					b = { fg = colors.gray, bg = colors.outerbg },
+					c = { fg = colors.gray, bg = colors.innerbg },
+				},
+				replace = {
+					a = { fg = colors.darkgray, bg = colors.replace, gui = "bold" },
+					b = { fg = colors.gray, bg = colors.outerbg },
+					c = { fg = colors.gray, bg = colors.innerbg },
+				},
+				normal = {
+					a = { fg = colors.darkgray, bg = colors.normal, gui = "bold" },
+					b = { fg = colors.gray, bg = colors.outerbg },
+					c = { fg = colors.gray, bg = colors.innerbg },
+				},
+				insert = {
+					a = { fg = colors.darkgray, bg = colors.insert, gui = "bold" },
+					b = { fg = colors.gray, bg = colors.outerbg },
+					c = { fg = colors.gray, bg = colors.innerbg },
+				},
+				command = {
+					a = { fg = colors.darkgray, bg = colors.command, gui = "bold" },
+					b = { fg = colors.gray, bg = colors.outerbg },
+					c = { fg = colors.gray, bg = colors.innerbg },
+				},
+			}
 
-            -- Setup Lualine with the custom theme
-            require("lualine").setup({
-                options = {
-                    theme = theme,
-                },
-                sections = {
-                    lualine_c = {
-                        { "filename", path = 1 }, -- 0 = just filename, 1 = relative path, 2 = absolute path
-                    },
-                },
-            })
-        end,
-    },
+			-- Setup Lualine with the custom theme
+			require("lualine").setup({
+				options = {
+					theme = theme,
+				},
+				sections = {
+					lualine_c = {
+						{ "filename", path = 1 }, -- 0 = just filename, 1 = relative path, 2 = absolute path
+					},
+					lualine_y = { "os.date('%a %d %b %H:%M')", "data", "require'lsp-status'.status()" },
+				},
+			})
+		end,
+	},
 }
-
