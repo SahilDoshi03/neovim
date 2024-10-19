72aa0e72ddaa8e8b9d28bcca2f2072b33bc83b8c
Sahil Doshi <sahildoshi@Sahils-MacBook-Pro.local>
2024-10-19 13:14:23 +0530

    Add text wrap and line selection

--- lua/remap.lua
+++ lua/remap.lua
@@ -3,10 +3,19 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
+-- select entire buffer
 vim.keymap.set("n","<C-a>","ggVG")
+-- select entire line
+vim.keymap.set("n","<C-l>","^v$")
+
+
 vim.keymap.set("n", "J", "mzJ`z")
+
+-- keep cursor in the center of the page when srolling up or down
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+-- keep cursor in the center when searching
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 vim.keymap.set("n", "<leader>zig", "<cmd>LspRestart<cr>")
@@ -31,10 +40,10 @@ vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
-vim.keymap.set("n", "<leader>ee", "oif err != nil {<CR>}<Esc>Oreturn err<Esc>")
+vim.keymap.set("n", "<leader>ee", "oif err != nil {<cr>}<esc>oreturn err<esc>")
 
 vim.keymap.set("n", "<leader>vpp", "<cmd>e ~/.dotfiles/nvim/.config/nvim/lua/theprimeagen/packer.lua<CR>")
-vim.keymap.set("n", "<leader>mr", "<cmd>CellularAutomaton make_it_rain<CR>")
+vim.keymap.set("n", "<leader>mr", "<cmd>CellularAutomaton make_it_rain<cr>")
 
 vim.keymap.set("n", "<leader><leader>", function()
     vim.cmd("so")
--- lua/set.lua
+++ lua/set.lua
@@ -5,21 +5,21 @@ vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
-
 vim.opt.smartindent = true
 
-vim.opt.wrap = false
+vim.opt.wrap = true
+vim.opt.linebreak = true
 
 vim.opt.swapfile = false
 vim.opt.backup = false
 
 local home_dir = os.getenv("USERPROFILE") or os.getenv("HOME")
 if home_dir then
-    -- Use the appropriate path separator based on the operating system
-    local path_separator = package.config:sub(1, 1)  -- Get the correct path separator
-    vim.opt.undodir = home_dir .. path_separator .. ".vim" .. path_separator .. "undodir"
+  -- Use the appropriate path separator based on the operating system
+  local path_separator = package.config:sub(1, 1) -- Get the correct path separator
+  vim.opt.undodir = home_dir .. path_separator .. ".vim" .. path_separator .. "undodir"
 else
-    print("Warning: HOME or USERPROFILE environment variable is not set.")
+  print("Warning: HOME or USERPROFILE environment variable is not set.")
 end
 
 vim.opt.undofile = true
