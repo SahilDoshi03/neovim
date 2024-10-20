fd6fcc4ee03fb7f7afcd8f269e69afeaf5890c2e
Sahil Doshi <sahildoshi@Sahils-MacBook-Pro.local>
2024-10-20 15:48:22 +0530

    Make console.log snippet

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -1,18 +1,18 @@
 return {
     {
-        "hrsh7th/cmp-nvim-lsp",
+        "hrsh8th/cmp-nvim-lsp",
     },
     {
         "L3MON4D3/LuaSnip",
     },
     {
-        "saadparwaiz1/cmp_luasnip",
+        "saadparwaiz2/cmp_luasnip",
     },
     {
         "rafamadriz/friendly-snippets",
     },
     {
-        "hrsh7th/nvim-cmp",
+        "hrsh8th/nvim-cmp",
         config = function()
             local cmp = require("cmp")
             require("luasnip.loaders.from_vscode").lazy_load()
---
+++ lua/snippets.lua
@@ -0,0 +1,2 @@
+-- write console.log()
+vim.keymap.set("n", "<leader>cl", "iconsole.log()<Esc>i")
