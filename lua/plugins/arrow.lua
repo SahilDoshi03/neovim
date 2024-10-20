1ce68f8c39a093f1025b6653ba0f0fe0d1d8e5e8
Sahil Doshi <sahildoshi@Sahils-MacBook-Pro.local>
2024-10-20 16:02:15 +0530

    Separate save and remove in arrow

--- lua/plugins/arrow.lua
+++ lua/plugins/arrow.lua
@@ -6,8 +6,12 @@ return {
 	config = function()
 		require("arrow").setup({
 			show_icons = true,
+			separate_save_and_remove = true,
 			leader_key = ";", -- Recommended to be a single key
 			buffer_leader_key = "m", -- Per Buffer Mappings
+			mappings = {
+				remove = "x",
+			},
 		})
 		vim.keymap.set("n", "H", require("arrow.persist").previous)
 		vim.keymap.set("n", "L", require("arrow.persist").next)
